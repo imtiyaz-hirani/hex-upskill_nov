3f9c1f04019972acd14ddf2b4d9366d57b81a597
imtiyaz-deepcoder <imtiyaz@deepcoder.dev>
2025-11-06 10:27:29 +0530

    Many TO many Reln and POST API for MtoM

---
+++ src/main/java/com/hex/trs/repository/PlanRepository.java
@@ -0,0 +1,7 @@
+package com.hex.trs.repository;
+
+import com.hex.trs.model.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanRepository extends JpaRepository<Plan,Long> {
+}
